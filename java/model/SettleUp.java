968acd9388a007e87911649b5b4dbea946fc6a83
i5haan <ishaankohli4321@gmail.com>
2018-06-28 10:27:16 +0530

    Added integer Id fields

--- java/model/SettleUp.java
+++ java/model/SettleUp.java
@@ -5,6 +5,20 @@ public class SettleUp {
 	private int grpid;
 	private String paidBy;
 	private String paidTo;
+	private int paidById;
+	private int padiToId;
+	public int getPaidById() {
+		return paidById;
+	}
+	public void setPaidById(int paidById) {
+		this.paidById = paidById;
+	}
+	public int getPadiToId() {
+		return padiToId;
+	}
+	public void setPadiToId(int padiToId) {
+		this.padiToId = padiToId;
+	}
 	public double getAmount() {
 		return amount;
 	}
