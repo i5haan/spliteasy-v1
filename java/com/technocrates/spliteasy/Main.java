14dbcf57a49c45ddd6a64dfe51bfec854bf718b2
i5haan <ishaankohli4321@gmail.com>
2018-06-29 15:11:14 +0530

    Test Main Class

--- java/com/technocrates/spliteasy/Main.java
+++ java/com/technocrates/spliteasy/Main.java
@@ -12,11 +12,13 @@ public class Main {
 	public static void main(String arg[])
 	{
 		ExpenseUtil e=new ExpenseUtil();
-		ArrayList<Integer> ratios=new ArrayList<>();
-		ratios.add(1);
-		ratios.add(1);
-		ratios.add(2);
-		boolean res=e.create(72, "Hello Whats Up", 26, 800,ratios);
+//		ArrayList<Integer> ratios=new ArrayList<>();
+//		ratios.add(1);
+//		ratios.add(1);
+//		ratios.add(2);
+//		boolean res=e.create(72, "Hello Whats Up", 26, 800,ratios);
+		boolean res=e.deleteExpence("130",107);
+		System.out.println(res);
 //		System.out.println(e.showAllSettleUP(7));
 	}
 }
